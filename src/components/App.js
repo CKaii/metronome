166634f35b8dc6b50ee9a3d5e8a7ba3655b06465
unknown <kevin.cheung.kai@gmail.com>
2019-03-19 16:59:15 -0400

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import '../metronome.css';
+import click1 from '../click1.wav'
+import click2 from '../click2.wav'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beastPerMeasure: 4
+    };
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+  }
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({ bpm })
+  }
+
+  startStop = () => {
+    if (this.state.playing) {
+      // stop the timer
+      clearInterval(this.timer);
+      this.setState({
+        playing: false
+      });
+    } else {
+      // start a timer with the current BPM
+      this.timer = setInterval(
+        this.playClick,
+        (60 / this.state.bpm) * 1000
+      );
+      this.setState(
+        {
+          count: 0,
+          playing: true
+          //Play a click "immediately" (after setState finishes)
+        },
+        this.playClick
+      )
+    }
+  }
+
+  playClick = () => {
+    const { count, beastPerMeasure } = this.state;
+
+    //The first beat will have a different sound than the others
+    if (count % beastPerMeasure === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    }
+
+    // Keeps track of whih beat we're on
+    this.setState( state => ({
+      count: (state.count + 1) % state.beatsPerMeasure
+    }));
+  };
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+
+    if (this.state.playing) {
+      // Stop the old timer and start a new one
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
+      // Set the new BPM, and reset the beat counter
+      this.setState({
+        count: 0,
+        bpm
+      });
+    } else {
+      this.setState({ bpm });
+    }
+  }
+
+  render() {
+    const { playing, bpm } = this.state;
+
+    return (
+      <div className='metronome'>
+        <div className='bpm-slider'>
+          <div>
+            {bpm} BPM
+          </div>
+          <input
+            type='range'
+            min='60'
+            max='240'
+            value={bpm}
+            onChange={this.handleBpmChange}
+          />
+        </div>
+        <button onClick={this.startStop}>
+          { playing ? 'Stop' : 'Start' }
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
